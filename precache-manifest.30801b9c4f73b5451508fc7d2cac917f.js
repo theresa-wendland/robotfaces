1ea16d40276bfd7ce36029aff9ad605a0f312bb0
Theresa <wendland.theresa@gmail.com>
2019-09-15 09:22:36 -0700

    Robo friends deployed

---
+++ precache-manifest.30801b9c4f73b5451508fc7d2cac917f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9d234fdd1a30490fb809205ffb4e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a44ae929ddbdf89a8843",
+    "url": "/static/css/main.4b55e72f.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "a44ae929ddbdf89a8843",
+    "url": "/static/js/main.1dce2b60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
